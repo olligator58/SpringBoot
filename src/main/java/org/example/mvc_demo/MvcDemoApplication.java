c47722e664372a91ad137c6af0a08655c6739b9f
Oleg <olligator58@gmail.com>
2022-10-28 12:51:04 +0300

    first commit

---
+++ src/main/java/org/example/mvc_demo/MvcDemoApplication.java
@@ -0,0 +1,15 @@
+package org.example.mvc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcDemoApplication.class, args);
+    }
+
+}
+
